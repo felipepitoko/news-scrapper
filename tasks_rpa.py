43ed06c69a3e55d71afc5dd75ee83b55539c7fc1
felipepitoko <lip-sousa@hotmail.com>
2024-09-10 00:59:33 -0300

    trying to get a webpage

--- tasks_rpa.py
+++ tasks_rpa.py
@@ -4,12 +4,9 @@ from robocorp import workitems
 from robocorp.workitems import Input
 from robot_rpa import RobotRpa
 
+@task
 def minimal_task():
     selenium = RobotRpa()
     selenium.set_webdriver()
     selenium.open_url("https://github.com/robocorp/rpaframework", "github.png")
     print("Done.")
-
-
-if __name__ == "__main__":
-    minimal_task()
