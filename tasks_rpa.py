ea9bcc6f27d435b3a7d8ffdbc3079a1bb03c4cfd
felipepitoko <lip-sousa@hotmail.com>
2024-09-10 01:50:12 -0300

    trying to get a webpage

--- tasks_rpa.py
+++ tasks_rpa.py
@@ -2,6 +2,7 @@ import re
 from robocorp.tasks import task
 from robocorp import workitems
 from robocorp.workitems import Input
+from robocorp.workitems import Output
 from robot_rpa import RobotRpa
 
 @task
@@ -10,4 +11,6 @@ def minimal_task():
     selenium.set_webdriver()
     selenium.open_url("https://www.google.com/", "google.png")
     selenium.driver_quit()
+    workitems.outputs.create()
+    Output.add_file("google.png", "google.png")
     print("Done.")
