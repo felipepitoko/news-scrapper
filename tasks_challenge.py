7ca4f75b687ff1917c7344d76f0eebfe2dedf360
felipepitoko <lip-sousa@hotmail.com>
2024-09-12 01:32:26 -0300

    testing rpa framework

--- tasks_challenge.py
+++ tasks_challenge.py
@@ -8,14 +8,16 @@ from helper_functions import *
 
 @task
 def minimal_task():
-    rpa = RpaNewsLatimes()
-    rpa.set_webdriver()
-    rpa.open_url("https://www.latimes.com/", "latimes.png")
-    rpa.search_content(search_phrase=input['search_phrase'])
-    rpa.sort_news_results(topic_sort_key=input['topic_sort_key'])
-    
-    rpa.get_news(max_months=input['months'])    
+    for item in workitems.inputs:
+        input = item.payload
+        rpa = RpaNewsLatimes()
+        rpa.set_webdriver()
+        rpa.open_url("https://www.latimes.com/", "latimes.png")
+        rpa.search_content(search_phrase=input['search_phrase'])
+        rpa.sort_news_results(topic_sort_key=input['topic_sort_key'])
+        
+        rpa.get_news(max_months=input['months'])    
 
-    rpa.driver_quit()
-    news_list = rpa.export_retrieved_news()
-    save_dict_to_xlsx(news_list, 'output/news_list.xlsx')
+        rpa.driver_quit()
+        news_list = rpa.export_retrieved_news()
+        save_dict_to_xlsx(news_list, 'output/news_list.xlsx')
