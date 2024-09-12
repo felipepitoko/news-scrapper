4131e20b4b264b6083d0aa55c4a5b184cbed1129
felipepitoko <lip-sousa@hotmail.com>
2024-09-12 10:28:55 -0300

    testing rpa framework

--- tasks_custom_selenium.py
+++ tasks_custom_selenium.py
@@ -1,10 +1,20 @@
 from robocorp.tasks import task
 from custom_selenium import CustomSelenium
+from robocorp import workitems
+import logging
 
 @task
 def minimal_task():
-    selenium = CustomSelenium()
-    selenium.set_webdriver()
-    selenium.open_url("https://www.latimes.com/", "latimes.png")
-    selenium.driver_quit()
-    print("Done.")
+    logger = logging.getLogger(__name__)
+
+    for item in workitems.inputs:
+        try:
+            logger.info("Processing item with data: %s", item.payload)
+            selenium = CustomSelenium()
+            selenium.set_webdriver()
+            selenium.open_url(item['access_link'], item['img_filename'])
+            selenium.driver_quit()
+            logger.info("Processing item with data: %s", item.payload)
+        except Exception as e:
+            logger.error("Error processing item with data %s: %s", item.payload, e)
+            raise
